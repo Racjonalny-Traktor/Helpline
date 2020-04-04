a6fb28db6c054435bdbdb00afd8c7dd1d13b9f0c
Konrad Pękala <kgpekala@gmail.com>
2020-04-05 00:49:44 +0200

    feat(home): added home screen

--- CharityHelp/mobile/app/src/main/java/com/example/charityhelp/ui/main/MainActivity.kt
+++ CharityHelp/mobile/app/src/main/java/com/example/charityhelp/ui/main/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.example.charityhelp.ui.main
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager.widget.PagerAdapter
 import com.example.charityhelp.R
 import com.example.charityhelp.ui.main.fragment.home.HomeFragment
 import com.example.charityhelp.ui.main.fragment.list.ListFragment
 import com.example.charityhelp.ui.main.fragment.map.MapFragment
 import com.example.charityhelp.ui.main.fragment.profile.ProfileFragment
-import com.example.charityhelp.ui.main.fragment.adapter.MainFragmentsAdapter
+import com.example.charityhelp.ui.main.fragment.adapter.PageAdapterMain
+import com.google.android.material.tabs.TabLayout
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -19,7 +22,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var mMapFragment: MapFragment
     private lateinit var mProfileFragment: ProfileFragment
 
-    private lateinit var mFragmentAdapter: MainFragmentsAdapter
+    private lateinit var mFragmentAdapter: PageAdapterMain
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,8 +32,26 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initTabLayout() {
-        mFragmentAdapter = MainFragmentsAdapter(supportFragmentManager,this)
+        mFragmentAdapter = PageAdapterMain(supportFragmentManager,this)
         viewPager.adapter = mFragmentAdapter
         tabLayout.setupWithViewPager(viewPager)
+        tabLayout.setSelectedTabIndicatorColor(Color.TRANSPARENT)
+
+        for (i in 0..tabLayout.tabCount-1) {
+            val tab = tabLayout.getTabAt(i)
+            tab!!.customView = mFragmentAdapter.getTabView(i)
+        }
+
+        tabLayout.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener{
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+            }
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+                mFragmentAdapter.setTabColor(false,tab!!.customView!!,applicationContext)
+            }
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                mFragmentAdapter.setTabColor(true,tab!!.customView!!,applicationContext)
+            }
+        })
+        viewPager.currentItem = 0
     }
 }
