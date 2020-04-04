90d9714cd17d9e73234de8621132b2f6a5b84baf
Konrad Pękala <kgpekala@gmail.com>
2020-04-05 01:32:20 +0200

    feat(map): added mapbox's map

--- CharityHelp/mobile/app/src/main/java/com/example/charityhelp/ui/main/fragment/map/MapFragment.kt
+++ CharityHelp/mobile/app/src/main/java/com/example/charityhelp/ui/main/fragment/map/MapFragment.kt
@@ -1,22 +1,33 @@
 package com.example.charityhelp.ui.main.fragment.map
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.fragment.app.Fragment
 import com.example.charityhelp.R
 import com.example.charityhelp.ui.main.MainActivity
+import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.maps.MapView
+import com.mapbox.mapboxsdk.maps.Style
+import com.mapbox.mapboxsdk.maps.Style.OnStyleLoaded
+import kotlinx.android.synthetic.main.fragment_map.*
+import kotlinx.android.synthetic.main.fragment_map.view.*
+
 
 class MapFragment(val mainActivity: MainActivity) : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_map, container, false)
+
+        view.mapView.onCreate(savedInstanceState)
+        view.mapView.getMapAsync { mapboxMap ->
+            mapboxMap.setStyle(Style.MAPBOX_STREETS,{ style: Style ->
+
+            })
+        }
+
+        return view
     }
 
 }
