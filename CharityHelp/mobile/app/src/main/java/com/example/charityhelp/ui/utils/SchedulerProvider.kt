4ea6650b5cf4cb652b806f7de31d7b925300f829
Konrad Pękala <kgpekala@gmail.com>
2020-04-05 04:31:05 +0200

    feat: added profile screen

---
+++ CharityHelp/mobile/app/src/main/java/com/example/charityhelp/ui/utils/SchedulerProvider.kt
@@ -0,0 +1,20 @@
+package com.example.charityhelp.ui.utils
+
+import io.reactivex.Scheduler
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+
+object SchedulerProvider {
+
+    fun io(): Scheduler {
+        return Schedulers.io()
+    }
+
+    fun ui(): Scheduler {
+        return AndroidSchedulers.mainThread()
+    }
+
+    fun computation(): Scheduler {
+        return Schedulers.computation()
+    }
+}
