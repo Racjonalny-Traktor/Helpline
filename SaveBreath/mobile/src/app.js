20b92561c16530a0702a858c9373d6004d79cf86
dbartoss <dawid.bartosz@miquido.com>
2020-04-05 07:54:01 +0200

    feat(App): Add new icon provider dependency matching design guidelines

--- SaveBreath/mobile/src/app.js
+++ SaveBreath/mobile/src/app.js
@@ -9,17 +9,21 @@
 import * as React from 'react';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
-import { ApplicationProvider } from '@ui-kitten/components';
+import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
+import { EvaIconsPack } from '@ui-kitten/eva-icons';
 import { mapping, dark as darkTheme } from '@eva-design/eva';
 
 import NavigationProvider from './navigation-provider';
 
 const App = () => (
-  <ApplicationProvider mapping={mapping} theme={darkTheme}>
-    <NavigationContainer>
-      <NavigationProvider />
-    </NavigationContainer>
-  </ApplicationProvider>
+  <React.Fragment>
+    <IconRegistry icons={EvaIconsPack} />
+    <ApplicationProvider mapping={mapping} theme={darkTheme}>
+      <NavigationContainer>
+        <NavigationProvider />
+      </NavigationContainer>
+    </ApplicationProvider>
+  </React.Fragment>
 );
 
 export default App;
